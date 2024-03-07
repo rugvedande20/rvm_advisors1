145ea531acab18fc0b459c97288cf0f32882b961
rugvedande20 <160820137+rugvedande20@users.noreply.github.com>
2024-03-07 09:20:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 <body>
     <header>
         <img src="logo.png" alt="Logo">
-        <a href="loginsystem.php"><button class="signin-btn">Sign in</button></a>
+        <a href="http://rvmadvisors.wuaze.com/loginsystem.php"><button class="signin-btn">Sign in</button></a>
     </header>
 
     <section>
         <pre><b>TAX REDUCTION
 HAS NEVER BEEN EASIER!</b></pre>
         <p>Revolutionize tax reduction with our cutting-edge software. Support our cause for a simplified tax process. Experience innovative solutions for financial empowerment. Join us in shaping the future of tax management.</p>
-        <a href="signup.php" class="register-button">Sign Up</a>
+        <a href="http://rvmadvisors.wuaze.com" class="register-button">Sign Up</a>
     </section>
 </body>
 </html>
